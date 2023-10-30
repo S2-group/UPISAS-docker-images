7d9e98a8496521651a9eac8c07daa431a359a9ed
EGAlberts <EGAlberts@github.com>
2023-10-30 18:57:56 +0100

    Cleanup + http patches + dingnet image

---
+++ src/ubuntu/install/dingnet/install_intellij.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
+wget -O intellij.tar.gz https://download.jetbrains.com/idea/ideaIU-2023.2.4.tar.gz?_ga=2.103962051.299580164.1698673927-1635547914.1698673927
+
+tar -xzf intellij.tar.gz -C ~/Desktop/
+
---
+++ src/ubuntu/install/dingnet/install_jdk.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -ex
+
+apt-get update
+
+apt-get install -y default-jdk default-jre
